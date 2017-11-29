f8137bd20e0ed4f0fc9f815fb7c47dccf9c119ec
Eun Il Kim <k.eun121@gmail.com>
2017-11-29 02:26:25 -0500

    Modifying ezcommand skeleton and adding test file

--- ezcommands.py
+++ ezcommands.py
@@ -1,17 +1,70 @@
 from User import *
-from Project imoport *
+from Project import *
 from Team import *
 from Bid import *
 from Task import *
 import numpy
 
+##############################################################################
+##############################################################################
+#NOTES:
+#
+#use this functions to ensure your classes are connected to essentials classes
+#also to make sure no duplicates and blacklisted user/project/bid... so forth
+#aren't created.
+
+##############################################################################
+##############################################################################
+#all classes
+#get attribute given id and keys
+def get_attribute():
+	pass
+	
+#prints table of all DB
+def print_all(DB):
+	pass
+	
+#prints all attribute of class
+def print_class(object):
+	pass
+	
+#prints a whole column of an attribute
+def print_col(DB, attribute):
+	pass
+##############################################################################
+#SU
+#takes required attribute of new user and places them in SU tasks
+def register_user():
+	pass
+
+#prints status of user: userType, status, warnings, and balance
+def user_report():
+	pass
+	
+##############################################################################
 #for users
+#will take what is required for 
+def start_project():
+	pass
+
+#must have client and project id for input
+#takes required feature for a bid and initiates a bid
+def start_bid():
+	pass	
+	
 def calc_avg_rating(object):
 	ratings = []
 	if object.__class__ == Team:
 		for user in object.get_dev_ids():
-			for rate in user.get_ratings()
+			for rate in user.get_ratings():
 				ratings.append(rate)
 	else:
 		ratings = object.get_ratings()
 	return max(numpy.mean(), 1)
+
+	#from,to,amount
+def tranfer_funcs(from_user, to_user, amount):
+	pass
+
+
+ 
---
+++ initial_data.py
@@ -0,0 +1,31 @@
+from User import *
+from Project import *
+from Team import *
+from Bid import *
+from Task import *
+import ezcommands
+import datetime 
+
+user1 = User(name = "Johnny Boy", username = "eats", password = "Dog", user_type = "developer")
+user2 = User(name = "Jane Doe", username = "isa", password = 'cat', user_type = "developer")
+user3 = User(name = "Cow", username = "can", password = 'code?', user_type = "developer")
+user4 = User(name = "Loner", username = "Loner", password = 'in_corner', user_type = "developer")
+
+t1 = Team(admin_ids = [user1.get_id()], dev_ids = [user1.get_id(),user2.get_id()], name = "WallStreet, where many things can go wrong")
+t2 = Team(admin_ids = [user3.get_id()], dev_ids = [user3.get_id()], name = "One Cow Team")
+t3 = Team(admin_ids = [user4.get_id()], dev_ids = [user4.get_id()], name = "Rainbow Darkness")
+
+client1 = User(name = "Jack Sparrow", username = "Fish", password = "cat", user_type = "client")
+client2 = User(name = "Jasmin Tea", username = "is", password = 'awesome!', user_type = "client")
+client3 = User(name = "Rick", username = "mr", password = 'meeseeks', user_type = "client")
+client4 = User(name = "Morty", username = "rick", password = 'adventure', user_type = "client")
+
+project1 = Project(client_id = client1.get_id(), title = "Hello", desc = "I have a lot of money", bid_id = now, team_id = t1.get_id(), start_date = now, end_date = now)
+project2 = Project(client_id = client2.get_id(), title = "World", desc = "I have a lot of money, too?", bid_id = now, team_id = t2.get_id(), start_date = now, end_date = now)
+project3 = Project(client_id = client3.get_id(), title = "Goodbye", desc = "I have more money than the other guy", team_id = t3.get_id(), bid_id = now, start_date = now, end_date = now)
+project4 = Project(client_id = client4.get_id(), title = "VR", desc = "Don't listen to any of them, I have the most", bid_id = now, start_date = now, end_date = now)
+
+bid1 = Bid(client_id = client1.get_id(),project_id = project1.get_id(), start_date = now, end_date=now)
+bid2 = Bid(client_id = client2.get_id(),project_id = project2.get_id(), start_date = now, end_date=now)
+bid3 = Bid(client_id = client3.get_id(),project_id = project3.get_id(), start_date = now, end_date=now)
+bid4 = Bid(client_id = client4.get_id(),project_id = project4.get_id(), start_date = now, end_date=now)
